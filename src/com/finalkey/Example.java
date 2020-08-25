0493a6d61ce4af400b7786c3dc384d4639d53340
sseela <s.sabelli@bstore.capgemini.com>
2020-08-25 09:45:22 +0530

    #Test commit

---
+++ src/com/finalkey/Example.java
@@ -0,0 +1,17 @@
+package com.finalkey;
+
+public class Example {
+		
+	private final int id;
+	
+	private final String name;
+		
+	public Example(int i, String j) {
+		this.id = i;
+		this.name = j;
+	}
+	public static void main(String[] args) {
+		Example ex = new Example(10, "sandeep");
+		
+	}
+}
